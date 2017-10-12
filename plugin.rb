08d8e65e4242437fab4f8731221c7ebe2499767c
Evg <budo@narod.ru>
2017-10-12 12:21:45 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,10 +1,9 @@
 # name: Toxu
-# about: add a detals site and dev
-# version: 0.2
+# about: 2 page layout
+# version: 0.1
 # authors:  Evg
-# url: https://github.com/Toxu-ru/Toxu
+# url: https://github.com/Toxu-ru/discourse-test
 
-register_asset "stylesheets/sidebar.scss"
 
 Discourse::Application.routes.append do
   get '/dev' => 'discourse_dev/dev#my_page'
@@ -12,24 +11,3 @@ Discourse::Application.routes.append do
 end
 
 load File.expand_path('../lib/dev/engine.rb', __FILE__)
-
-after_initialize do
-
-  require 'listable_topic_serializer'
-  class ::ListableTopicSerializer
-
-    def excerpt
-      accepted_id = object.custom_fields["accepted_answer_post_id"].to_i
-      if accepted_id > 0
-        cooked = Post.where(id: accepted_id).pluck('cooked')
-        PrettyText.excerpt(cooked[0], 150, {})
-      else
-        object.excerpt
-      end
-    end
-
-    def include_excerpt?
-      true
-    end
-  end
-end
